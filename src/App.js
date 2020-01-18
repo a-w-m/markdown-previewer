9178b03caba15e0632d076e0e02e3f9cb06c0c93
a-w-m <mamrey@gmail.com>
2020-01-18 15:56:36 -0500

    Implement style layout with css grid

--- src/App.js
+++ src/App.js
@@ -4,12 +4,43 @@ import "./App.css";
 import { findByLabelText } from "@testing-library/react";
 import marked, { parser } from "marked";
 
+const defaultText = `# Live Markdown Previewer
+ ## Markdown 
+ 
+ Markdown is a popular [markup language](https://en.wikipedia.org/wiki/Markdown).  
+ 
+ Simply display inline code: \`console.log(displaying inline code)\`
+
+ or multi-line code blocks: 
+ 
+ \`\`\`
+ function square(a){  
+	 (return a*a)  
+	}	
+\`\`\`
+
+ **bold** or _italicize_
+
+ > block quote
+
+ Make lists 
+  -bulleted
+    -with indents
+
+1. Or numbered
+2. lists
+
+You can also embed images:
+
+![alt text](https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/205_Markdown_logo_logos-512.png " Markdown logo")
+ `;
+
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			markdown: "",
+			markdown: defaultText,
 			displayPreview: "live"
 		};
 
@@ -44,49 +75,55 @@ class App extends Component {
 	};
 
 	render() {
-		const helloWorld = "hello world, we're creating a Markdown Previewer";
 		return (
 			<div className="App">
-				{helloWorld}
-
-				<Editor value={this.state.markdown} onChange={this.handleEditorChange}>
-					Editor
-				</Editor>
+				<div class ="title">Markdown Editor</div>
+				<Button
+					type="button"
+					onClick={this.handleClearPreviewClick}
+					className="clearButton"
+				>Clear</Button>
 				<PreviewDropDown
 					value={this.state.displayPreview}
 					onChange={this.handlePreviewDropDownChange}
 					type="select"
+					className="previewDropdown"
 				></PreviewDropDown>
-
+				<Editor
+					value={this.state.markdown}
+					onChange={this.handleEditorChange}
+					className = "editor"
+				>
+				</Editor>
 				<Preview
 					displayPreview={this.state.displayPreview}
 					parsedHTML={this.state.markdown}
+					className="preview"
 				/>
 
-				<Button type="button" onClick={this.handleClearPreviewClick}>
-					Clear Preview
-				</Button>
-				<Input ref={this.fileInput} onChange={this.handleFileInputSubmit} />
+				<Input ref={this.fileInput} onChange={this.handleFileInputSubmit} className ="fileInput"/>
+				<div class ="footer">Markdown previewer created by awm</div>
+
 			</div>
 		);
 	}
 }
 
 const Editor = props => {
-	const { value, onChange, children } = props;
+	const { value, onChange, className, children } = props;
 	return (
-		<div>
+		<div className={className}>
 			<div>{children}</div>
-			<textarea value={value} onChange={onChange}></textarea>
+			<textarea value={value} onChange={onChange} ></textarea>
 		</div>
 	);
 };
 
 const PreviewDropDown = props => {
-	const { value, onChange } = props;
+	const { value, onChange, className } = props;
 	return (
-		<form>
-			<select value={value} onChange={onChange}>
+		<form className={className}>
+			<select value={value} onChange={onChange} >
 				<option value="live">Live Preview</option>
 				<option value="html">HTML Source</option>
 			</select>
@@ -95,33 +132,33 @@ const PreviewDropDown = props => {
 };
 
 const Preview = props => {
-	const { displayPreview, parsedHTML } = props;
+	const { displayPreview, parsedHTML, className } = props;
 	return (
-		<div>
+		<div className={className}>
 			{displayPreview == "live" ? (
-				<div
+				<div 
 					dangerouslySetInnerHTML={{
 						__html: marked(parsedHTML, { breaks: "true" })
 					}}
 				/>
 			) : (
-				<div>{marked(parsedHTML, { breaks: "true" })}</div>
+				<div >{marked(parsedHTML, { breaks: "true" })}</div>
 			)}
 		</div>
 	);
 };
 
 const Button = props => {
-	const { type, onClick, children } = props;
+	const { type, onClick, className, children } = props;
 	return (
-		<button type={type} onClick={onClick}>
+		<button type={type} onClick={onClick} className={className}>
 			{children}
 		</button>
 	);
 };
 
 const Input = React.forwardRef((props, ref) => (
-	<div>
+	<div className = {props.className}>
 		<label for="file"></label>
 		<input type="file" id="file" ref={ref} onChange={props.onChange} />
 	</div>
